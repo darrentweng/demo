33e0d28c0cb439e8f6b560495c4b869e18c84f02
Darren Weng <darren.t.weng@gmail.com>
2022-09-25 11:03:16 -0400

    diffpages

---
+++ pages/08_SocialMedia.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('Check out my social media!')
+
+st.markdown('My insta: [@dimondho](https://www.instagram.com/dimondho/).')
+st.markdown('My github: [darrentweng](https://github.com/darrentweng).')
